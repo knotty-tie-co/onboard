4a34ee4e380909b90ebcf6a2462a232260249c47
Mark Johnson <markjohnson303@gmail.com>
2016-11-08 10:34:52 -0700

    refactored steps to different urls

--- public/js/appRoutes.js
+++ public/js/appRoutes.js
@@ -2,10 +2,18 @@ angular.module('appRoutes', ['ui.router']).config(
     function($stateProvider, $urlRouterProvider, $locationProvider) {
       $urlRouterProvider.otherwise('/');
       $stateProvider
+
         .state('create-profile', {
           url: '/create-profile',
           templateUrl: './views/create-profile.html',
           controller: 'ProfileController'
+        })
+
+        .state('create-profile-step-1', {
+          url: '/create-profile/step-1',
+          templateUrl: './views/create-profile-step-1.html',
+          controller: 'ProfileController'
         });
+
         $locationProvider.html5Mode(true);
      });
